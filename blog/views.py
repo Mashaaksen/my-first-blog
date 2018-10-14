55a90bf2b278a2c8447d80b444f5378ff5064764
maksenov <ariaksenova@gmail.com>
2018-10-14 17:45:40 +0300

    without filter

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.contrib.auth.models import User
 
 def post_list(request):
-	me = User.objects.get(username='maksenov')
-	posts = Post.objects.filter(author=me)
+	posts = Post.objects.all()
 	return render(request, 'blog/post_list.html', {'posts': posts})
